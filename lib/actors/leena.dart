2999b4df5dda6d6ccffb93ad504070aac0f4861b
Nehal Jaisalmeria <nehal.jaisalmeria14@gmail.com>
2022-08-20 14:51:59 +0530

    First Commit

---
+++ lib/actors/leena.dart
@@ -0,0 +1,30 @@
+import 'package:flame/collisions.dart';
+import 'package:flame/components.dart';
+import 'package:leena/main.dart';
+import 'package:leena/world/ground.dart';
+
+class Leena extends SpriteComponent
+    with CollisionCallbacks, HasGameRef<LeenaGame> {
+  Leena() : super() {
+    debugMode = true;
+  }
+
+  bool onGround = false;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+    add(RectangleHitbox());
+  }
+
+  @override
+  void onCollision(intersectionPoints, other) {
+    super.onCollision(intersectionPoints, other);
+
+    if (other is Ground) {
+      gameRef.velocity.y = 0;
+      print('hit ground');
+      onGround = true;
+    }
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flame/game.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+import 'package:flutter/material.dart';
+import 'package:leena/actors/leena.dart';
+import 'package:leena/world/ground.dart';
+import 'package:tiled/tiled.dart';
+
+void main() {
+  runApp(GameWidget(game: LeenaGame()));
+}
+
+class LeenaGame extends FlameGame with HasCollisionDetection {
+  Leena leena = Leena();
+  double gravity = 1.8;
+  Vector2 velocity = Vector2(0, 0);
+  late TiledComponent homeMap;
+
+  @override
+  Future<void> onLoad() async {
+    await super.onLoad();
+
+    // add(SpriteComponent()
+    //   ..sprite = await loadSprite('background.png')
+    //   ..size = size);
+
+    homeMap = await TiledComponent.load('map.tmx', Vector2.all(32));
+    add(homeMap);
+
+    double mapWidth = 32.0 * homeMap.tileMap.map.width;
+    double mapHeight = 32.0 * homeMap.tileMap.map.height;
+
+    final obstacleGroup = homeMap.tileMap.getLayer<ObjectGroup>('ground');
+
+    for (final obj in obstacleGroup!.objects) {
+      add(
+        Ground(
+          size: Vector2(obj.width, obj.height),
+          position: Vector2(obj.x, obj.y),
+        ),
+      );
+      print(obj.x);
+    }
+
+    camera.viewport = FixedResolutionViewport(Vector2(mapWidth, mapHeight));
+
+    leena
+      ..sprite = await loadSprite('girl.png')
+      ..size = Vector2.all(200)
+      ..position = Vector2(200, 90);
+
+    add(leena);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    if (leena.y < size[1] - leena.height) {
+      velocity.y += gravity;
+      leena.position.y += velocity.y * dt;
+    }
+  }
+}
